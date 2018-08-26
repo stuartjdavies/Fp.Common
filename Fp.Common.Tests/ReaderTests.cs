9579640ff8b532a0876e0a8224023a452c4aaea9
stuartjdavies <stuart_j_davies@hotmail.com>
2018-08-26 22:22:01 +1000

    Update reader tests

---
+++ Fp.Common.Tests/ReaderTests.cs
@@ -0,0 +1,17 @@
+﻿using Fp.Common.Monads.Reader;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Fp.Common.Tests
+{
+    public class ReaderTests
+    {
+        [Fact]
+        public void Reader_Verify()
+        {
+           //R
+        }
+    }
+}
